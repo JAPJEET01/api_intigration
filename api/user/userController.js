9469c06574554a5ecbc0b6b66cf947539ee47cf5
JAPJEET SINGH <95476734+JAPJEET01@users.noreply.github.com>
2023-07-10 23:01:02 +0530

    nodejs

--- api/user/userController.js
+++ api/user/userController.js
@@ -1,5 +1,7 @@
 const user  =  require("./userScherma")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
 exports.login =  (req,res)=>{
     user.findOne({email:req.body.email})
     .then(data=>{
@@ -12,11 +14,21 @@ exports.login =  (req,res)=>{
         }
         else{
             if (bcrypt.compareSync(req.body.password ,  data.password)) {
+                let payload = {
+                    name:data.name,
+                    email:data.email,
+                    usertype:data.usertype,
+                    student_id:data.student_id,
+                    teacher_id:data.teacher_id
+                }  
+                token = jwt.sign(payload,"japjeet" , ({expiresIn:60*60*24*365}))
                 res.json({
                     status:200,
                     success:true,
                     messsage : "login successful",
+                    token : token  
                 })
+                
                 
             }
             else{
